a708e0dbe89fff76050b95f39ea3735b9c45009e
Kristofer Heyward <krisroc7@gmail.com>
2017-03-13 15:07:57 +0000

    My Message

---
+++ script.js
@@ -0,0 +1,17 @@
+TinyTurtle.apply(window);
+forward(40);
+right(90);
+forward(90);
+right(90);
+forward(40);
+right(90);
+forward(90);
+stamp();
+
+right(90);
+forward(40);
+right(50);
+forward(57);
+right(78);
+forward(59);
+stamp();
